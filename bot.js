12c03dcd331e42de53547757e828b277c1654d28
jamalbabia <47610404+jamalbabia@users.noreply.github.com>
2019-02-23 04:06:58 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -230,7 +230,7 @@ function play(guild, song) {
 }
  
 const adminprefix = "C";
-const devs = ['474354424391663616'];
+const devs = ['538411861637595136'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
